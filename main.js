23879f0fb73ffcae5ca6ba90adc2a9e026360cd1
Lirca Jimenez <lirca.je@gmail.com>
2021-11-10 14:21:35 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const contobox = document.querySelector(".contobox-container");
+const modal = document.querySelector(".modal-outer");
+const closeBtn = document.querySelector(".close-btn");
+const dvd = document.querySelector(".dvd");
+const text = document.querySelector(".text-1");
+const bottom = document.querySelector(".content-bottom");
+
+const mouseRollOver = () => {
+  let mouseEnterTimer;
+  contobox.addEventListener("mouseenter", () => {
+    mouseEnterTimer = setTimeout(() => {
+      console.log("mouse enter");
+      modal.classList.add("open");
+    }, 3000);
+    // apply animations when opening modal
+    dvd.style.animation = "ps3-ps4 3s linear 2s 1";
+    text.style.animation = "slide-text 3s ease-in-out 5s 1 forwards";
+    bottom.style.animation = "bottom 1s linear 8s 1 forwards";
+  });
+};
+
+const closeModal = () => {
+  modal.classList.remove("open");
+};
+
+contobox.addEventListener("mouseenter", mouseRollOver);
+closeBtn.addEventListener("click", closeModal);
+modal.addEventListener("click", (ev) => {
+  const isOutside = !ev.target.closest(".modal-inner");
+  if (isOutside) {
+    closeModal();
+  }
+});
+window.addEventListener("keydown", (ev) => {
+  if (ev.key === "Escape") {
+    closeModal();
+  }
+});
